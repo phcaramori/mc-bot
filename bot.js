a126b75066645508621f5e729aecbd3bd32cc2ac
phcaramori <31902152+phcaramori@users.noreply.github.com>
2021-02-04 13:20:40 -0300

    project restart

--- bot.js
+++ bot.js
@@ -14,24 +14,16 @@ client.on('message',message =>{
     const command = args.shift().toLowerCase();
 
     if(command == "help"){
-        message.channel.send('Do ``-shophelp`` for help with shops and ``-datahelp`` for help wth the server datapacks. You can also do ``-seed`` to get the server seed and ``-ip`` for the ip.')
+        message.channel.send('Do ``-shophelp`` for help with shops and ``-datahelp`` for help wth the server datapacks. You can also do ``-seed`` to get the server seed and ``-ip`` for the ip. Finally, You can also use ``-online`` to get a list of what players are currently on the server.')
         //end of -help
 
     }else if(command == "shophelp"){
         message.channel.send("Use this link for help with setting up a shop: https://github.com/Shopkeepers/Shopkeepers-Wiki/wiki/Player-Shop-Setup. \n \n if you prefer a video tutorial: https://drive.google.com/file/d/1bCkEd1eRRrp7-0aErHWyzz5PMUSeeTur/view?usp=sharing \n \n You can make the shopkeeper almost any passive mob.(Cow, sheep, cat, and much more.) I suggest that you use a villager for most shops, but you can also use shop-appropriate mobs such as using an iron golem for a iron shop or a cow for a food shop. \n \n If you have any questions, feel free to dm Walnut_.")
         //end of -shophelp
 
-    }else if(command == "datahelp"){
-        message.channel.send("for a list of datapacks in the server, do ``/datapack list``. \n To use the spectator mode datapack, do ``/trigger .spectator``. That command toggles you in and out of spectator.")
-        //end of -datahelp
-
     }else if(command == "ip"){
-        message.channel.send(`Server IP: thiccniggasandanimebiches.pvp.host`)
+        message.channel.send(`Server IP: 170.81.41.61.ipv4.reishosting.com.br:26056`)
         //end of -ip
-
-    }else if(command == "seed"){
-        message.channel.send(`Server seed: 6158718709991965043`)
-        //end of -seed
 
     }else if(command == "online"){ 
         const embed = new Discord.MessageEmbed()
@@ -41,7 +33,7 @@ client.on('message',message =>{
         //embed start
         async function run(){
             async function getData(){
-                const api_url= 'https://api.mcsrvstat.us/2/thiccniggasandanimebiches.pvp.host'
+                const api_url= 'https://api.mcsrvstat.us/2/170.81.41.61.ipv4.reishosting.com.br:26056'
                 let settings = { method: "Get" }
                 fetch(api_url, settings)
                 .then(res => res.json())
@@ -58,7 +50,7 @@ client.on('message',message =>{
                         embed.setFooter("updates every 10 minutes")
                         //
                     }else{
-                        message.channel.send("something went very, very wrong. TAKE ME OUT OF MY MISERY WALNUT")
+                        message.channel.send("something went very, very wrong.")
                     }
                     embed.setColor('#f7ae2f')
                     message.channel.send(embed)
@@ -68,6 +60,10 @@ client.on('message',message =>{
         } 
         run()
         //end of -online
+
+    }else if(command == "ping"){
+        message.channel.send("pong")
+        //end of -ping
     }else{
         message.channel.send("Invalid command. Do -help for a list of commands.")
     }
