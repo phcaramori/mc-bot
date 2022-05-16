373b7a2257165eeb866b63d8790b4bdfcfb90f3d
Pedro Caramori <phcaramori@gmail.com>
2022-05-16 19:28:54 -0300

    updated .gitignore

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
 export default
 {
-    "token": "NzQyNzk3MTI1ODQyMTA4NDU3.GkKH_Z.qSBEixTyUSL2c8E4Nj-TVyCmRVJwxfjivEPuUY-"
+    "token": "NzQyNzk3MTI1ODQyMTA4NDU3.GkKH_Z.qSBEixTyUSL2c8E4Nj-TVyCmRVJwxfjivEPuUY"
 }
