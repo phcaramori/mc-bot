aae7e9b281f518d399f5536277b4a198366adfc1
Pedro Caramori <phcaramori@gmail.com>
2022-06-16 01:52:21 -0300

    fized owoify imports

--- commands/text-modulation.js
+++ commands/text-modulation.js
@@ -1,16 +1,12 @@
+import _owoify from "owoify-js";
+let owoify = _owoify.default
 function owo(message) {
-    const owoify = require("owoify-js").default;
-
     let owoified = message.content.slice(5) //remove first 5 characters of the message (Ex: "-owo hello dude" turns into "hello dude")
-        message.delete();
-        message.channel.send(owoify(owoified, "owo")); 
+    message.channel.send(owoify(owoified, "owo")); 
 }
 function uwu (message) {
-    const owoify = require("owoify-js").default;
-
     let owoified = message.content.slice(5) //remove first 5 characters of the message (Ex: "-owo hello dude" turns into "hello dude")
-        message.delete();    
-        message.channel.send(owoify(owoified, "uwu")); 
+    message.channel.send(owoify(owoified, "uwu")); 
 }
 
 export { owo };
