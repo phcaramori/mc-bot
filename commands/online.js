8887f94a539280ac4cf43520d95d29c6d032fe6a
Pedro Caramori <phcaramori@gmail.com>
2022-08-18 02:19:04 -0300

    removed redundant async functions

--- commands/online.js
+++ commands/online.js
@@ -6,38 +6,31 @@ export default function (message) {
     let finalMessage = '';
     var playersOnline;
     var numOfPlayersOnline;
-    async function run() {
-        async function getData() {
-
-        //getting JSON results from API
-        const api_url = 'https://api.mcsrvstat.us/2/' + SERVER_SETTINGS.ip;
-        let settings = { method: "Get" };
-        fetch(api_url, settings)
-            .then(res => res.json())
-            .then((json) => {
-                //making the embed with the results
-                playersOnline = json.players.list;
-                numOfPlayersOnline = json.players.online;
-
-                if (numOfPlayersOnline) { //if there is any1 online
-                    playersOnline.forEach(element => {
-                        finalMessage = finalMessage + ' \n' + element;
-                    });
-                    embed.addField('Players on-line:', finalMessage);
-                    embed.setFooter("updates every 10 minutes");
-                    embed.setColor('#f7ae2f');
-                }
-                else if (!numOfPlayersOnline) { //if there is no-one online      
-                    embed.setTitle("The server is currently empty.");
-                    embed.setFooter("updates every 10 minutes");
-                    embed.setColor('#ff0000');
-                }
-                
-                message.channel.send(embed);
+    //getting JSON results from API
+    const api_url = 'https://api.mcsrvstat.us/2/' + SERVER_SETTINGS.ip;
+    let settings = { method: "Get" };
+    fetch(api_url, settings)
+    .then(res => res.json())
+    .then((json) => {
+        //making the embed with the results
+        playersOnline = json.players.list;
+        numOfPlayersOnline = json.players.online;
+        
+        if (numOfPlayersOnline) { //if there is any1 online
+            playersOnline.forEach(element => {
+                finalMessage = finalMessage + ' \n' + element;
             });
+            embed.addField('Players on-line:', finalMessage);
+            embed.setFooter("updates every 10 minutes");
+            embed.setColor('#f7ae2f');
         }
-        await getData();
-    }
-    run();
-    //end of -online
+        else if (!numOfPlayersOnline) { //if there is no-one online      
+            embed.setTitle("The server is currently empty.");
+            embed.setFooter("updates every 10 minutes");
+            embed.setColor('#ff0000');
+        }      
+
+        message.channel.send(embed);
+    });
 }
+ 
