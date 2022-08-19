5465e0d99d0063051fb4129ce43cb951946192d2
Pedro Caramori <phcaramori@gmail.com>
2022-08-18 21:01:12 -0300

    refactoring

--- settings.js
+++ settings.js
@@ -1,4 +1,10 @@
-export default {
+const MC_SERVER_SETTINGS = {
     "ip" : "br-6.purplehost.com.br:10245",
-    "seed" : "seed"
+    "seed" : "seed",
+    "version" : "1.18"
 }
+const BOT_SETTINGS = {
+    "prefix" : "-",
+}
+
+export {MC_SERVER_SETTINGS, BOT_SETTINGS};
