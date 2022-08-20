3ec348826fc04613f07937e39385cf40145bb9e2
Pedro Caramori <phcaramori@gmail.com>
2022-08-20 00:32:25 -0300

    updated to new EmbedBuilder

--- commands/help.ts
+++ commands/help.ts
@@ -1,16 +1,18 @@
-import  {MessageEmbed} from 'discord.js';
+import  { EmbedBuilder } from 'discord.js';
 
 export default {
     name: "help",
     aliases: ["h"],
     description: "Generates this message. Hi!",
     command: function (message) {
-        const embed = new MessageEmbed();
+        const embed = new EmbedBuilder();
         embed.setTitle("Help");
         //embed.addField("-shophelp", "Used for help with setting up shops");
-        embed.addField("-seed", "Used to find the world's seed");
-        embed.addField("-ip", "Used to find the server's ip");
-        embed.addField("-online", "Used to get a list of what players are currently on the server");
+        embed.addFields(
+            {name : "-seed", value: "Used to find the world's seed"},
+            {name : "-ip", value: "Used to find the server's ip"},
+            {name : "-online", value: "Used to get a list of what players are currently on the server"},
+        );
         //embed.addField("-set", "Used to set properties for the bot. Usage: ```-set [seed/ip] (value)```");
         embed.setColor('#7f7f7f');
         message.channel.send(embed);
