a89b7464ea84c1b399cde75d5819d236b64769f1
Pedro Caramori <phcaramori@gmail.com>
2022-08-20 01:29:40 -0300

    updated token to come from .env, updated Client creation syntax to math v 14 of discord.js

--- bot.ts
+++ bot.ts
@@ -1,32 +1,37 @@
 //externals
 import fs from 'fs'
 import DiscordJS, { Client , GatewayIntentBits } from 'discord.js';
-import {MC_SERVER_SETTINGS , BOT_SETTINGS} from './settings.js';
-import { TOKEN } from './auth.js';
+import {MC_SERVER_SETTINGS , BOT_SETTINGS} from './settings';
+import dotenv from 'dotenv';
+dotenv.config()
 
 // startup commands 
 let COMMANDS_LIST: any = [];
 fs.readdirSync("commands").forEach(async function(file) {
     let a = await import ("./commands/" + file);
-    COMMANDS_LIST.push(a);
+    COMMANDS_LIST.push(a.default);
     console.log("LOADED: " + a.default.name)
 }) 
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds , GatewayIntentBits.GuildMessages] });
-
-
-const prefix = BOT_SETTINGS.prefix;
+const client = new Client({ 
+    intents: [
+        GatewayIntentBits.Guilds, 
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ] 
+});
+const prefix = "-";
 client.on('ready', () => {
     console.log("bot online")
 })
 
-client.on('messageCreate', message =>{
+client.on('messageCreate', (message) =>{
     if(!message.content.startsWith(prefix)) return; //if message doesn't begin with the prefix, stop.
     let args = message.content.slice(prefix.length).split(/ +/); //returns array with all words in the command. Ex: ["help","how","to","do","this"]
     const inputCommand = args.shift().toLowerCase(); //returns only the first word in the command. Ex: "help"
-
+    console.log(inputCommand)
     //start of commands
-    COMMANDS_LIST.forEach(current => {
+    COMMANDS_LIST.forEach( current => {
         if(inputCommand === current.name){
             current.command(message,args)
         }else if(current.aliases){
@@ -41,4 +46,4 @@ client.on('messageCreate', message =>{
     });
 });
 
-client.login(TOKEN)
+client.login(process.env.TOKEN)
