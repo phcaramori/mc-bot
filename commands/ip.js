9c577d3555906e082dc18e7bfa824f300828f048
Pedro Caramori <phcaramori@gmail.com>
2024-06-25 03:33:28 -0300

    implemented ip command

---
+++ commands/ip.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('discord.js');
+const guildProfileSchema = require('../models/guildProfileSchema')
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+	.setName('ip')
+	.setDescription("Returns the IP for this discord server's linked minecraft server."),
+	async execute(interaction) {
+		await interaction.deferReply();
+
+		//get seed from guild settings on mongodb
+		let guildData;
+		try {
+			guildData = await guildProfileSchema.findOne({guildID: interaction.guildID})
+			if(!guildData){
+				interaction.editReply("```This bot has not yet been configured on this discord server. Ask an admin to do so with /configure-server-settings```");
+				return;
+			}else{
+                interaction.editReply("Server IP Adress: ```" + guildData.IP + "```");
+            }
+		} catch (err) {
+			console.log(err);
+		}
+	} 
+}       
