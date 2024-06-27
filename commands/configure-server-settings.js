a7bf93f7fd64024e8b01f28541eed7df96129a4e
Pedro Caramori <phcaramori@gmail.com>
2024-06-27 20:55:33 -0300

    implemented easy-confirmation to bot msgs requiring confirmation

--- commands/configure-server-settings.js
+++ commands/configure-server-settings.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const guildProfileSchema = require('../models/guildProfileSchema')
+const commandConfirmation = require('discord-js-easy-confirmations')
 
 
 module.exports = {
@@ -26,6 +27,7 @@ module.exports = {
 		const newServerIP = interaction.options.getString('ip-adress');
 		const newServerSeed = interaction.options.getInteger('seed') ?? null;
 		const newServerSeedStr = (newServerSeed) ? newServerSeed.toString() : "No Seed Set.";
+
 		let embed = new EmbedBuilder();
 		embed.setColor('#0080FF')
 		embed.setTitle("Confirm changes to bot settings?")
@@ -33,64 +35,39 @@ module.exports = {
 		let storedServerSeed = (guildData && guildData.Seed) ? guildData.Seed : "no seed set" ;
 		embed.addFields({name:"Server IP:", value: `**FROM:** ${storedServerIP} **TO:** ${newServerIP}`})
 		embed.addFields({name:"Server Seed:", value: `**FROM:** ${storedServerSeed.toString()} **TO:** ${newServerSeedStr}`})
-		
-		const confirm = new ButtonBuilder()
-			.setCustomId('confirmNewServerSettings')
-			.setLabel('Confirm')
-			.setStyle(ButtonStyle.Success);
-		const cancel = new ButtonBuilder()
-			.setCustomId('cancelNewServerSettings')
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Danger);
-		const row = new ActionRowBuilder()
-			.addComponents(confirm, cancel);
-		const confirmationMsg = await interaction.editReply({embeds: [embed], components: [row]}); 
+		await interaction.editReply({embeds: [embed]});
 
-		const collectorFilter = i => i.user.id === interaction.user.id; //make sure the incoming interaction cames from the same user who started the slash command
-		let resultEmbed = new EmbedBuilder();
-		try {
-			const confirmation = await confirmationMsg.awaitMessageComponent({ filter: collectorFilter, time: 60_000 }); //await response TODO TEST
-
-			if (confirmation.customId === 'confirmNewServerSettings') {
-				if(!guildData){ //if the db doesnt yet have information on this guild's settings
-					console.log("Creating new DB entry for guild " + interaction.guildId);
-					guildData = await guildProfileSchema.create({
-						guildId: interaction.guildId,
-						IP: newServerIP,
-						Seed: newServerSeed, //if no seed was given in the command, newServerSeed is already set to null.
-					})
-				}else{
-					await guildProfileSchema.findOneAndUpdate(
-						{guildId: interaction.guildId},
-						{
-							$set: {
-								IP: newServerIP,
-								Seed: newServerSeed,
-				}})}
-				
-				resultEmbed.setColor("#78F72F")
-				resultEmbed.setTitle("Saving Successful! The following changes were made:")
-				resultEmbed.addFields({name:"Server IP:", value: newServerIP, inline:true})
-				resultEmbed.addFields({name:"Server Seed:", value: newServerSeedStr, inline:true})
-
-			} else if (confirmation.customId === 'cancelNewServerSettings') {
+		//confirmation
+		commandConfirmation(interaction).then(async res => {
+			let resultEmbed = new EmbedBuilder();
+			if(res == "confirmed"){
+				try {
+					if(!guildData){ //if the db doesnt yet have information on this guild's settings
+						console.log("Creating new DB entry for guild " + interaction.guildId);
+						guildData = await guildProfileSchema.create({
+							guildId: interaction.guildId,
+							IP: newServerIP,
+							Seed: newServerSeed, //if no seed was given in the command, newServerSeed is already set to null.
+						})
+					}else{
+						await guildProfileSchema.findOneAndUpdate( {guildId: interaction.guildId} , {$set: { IP: newServerIP, Seed: newServerSeed } } )
+					}
+					resultEmbed.setColor("#78F72F")
+					resultEmbed.setTitle("Saving Successful! The following changes were made:")
+					resultEmbed.addFields({name:"Server IP:", value: newServerIP, inline:true})
+					resultEmbed.addFields({name:"Server Seed:", value: newServerSeedStr, inline:true})
+				} catch (err) {
+					console.log(err);	
+				}
+			}else if(res == "canceled"){
 				resultEmbed.setColor("#FF0000");
 				resultEmbed.setTitle("Operation canceled. No changes were made.")
-			}
-			await confirmation.update({ embeds: [resultEmbed], components: [] });
-		} catch (e) {
-			if(e.code=="InteractionCollectorError"){ //probably a timeout? no specific error code for timeout, might have to change if Ifind that this code can occur with other errors.
+			}else if(res == "timeout"){
 				resultEmbed.setColor("#FF0000");
 				resultEmbed.setTitle("Confirmation not recieved within 1 minute. No changes were made.");
 				resultEmbed.setTimestamp();
-				await interaction.editReply({ embeds: [resultEmbed], components: [] });
-			}else{
-				console.log(e);
-				resultEmbed.setColor("#FF0000");
-				resultEmbed.setTitle("Internal bot error occured. Please contact the developer and give the timestamp below");
-				resultEmbed.setTimestamp();
-				await interaction.editReply({ embeds: [resultEmbed], components: [] });
 			}
-		}
-	} 
+			await interaction.editReply({ embeds: [resultEmbed]});
+		})
+	}
 }       
--- commands/delete-all-server-settings.js
+++ commands/delete-all-server-settings.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder, ButtonBuilder, ActionRowBuilder, ButtonStyle } = require('discord.js');
 const guildProfileSchema = require('../models/guildProfileSchema')
+const commandConfirmation = require('discord-js-easy-confirmations')
 
 
 module.exports = {
@@ -16,52 +17,28 @@ module.exports = {
 			console.log(err);
 		}
 		
-		const confirm = new ButtonBuilder()
-			.setCustomId('confirmDeleteGuildData')
-			.setLabel('Confirm Delete Data')
-			.setStyle(ButtonStyle.Danger);
-		const cancel = new ButtonBuilder()
-			.setCustomId('cancelDeleteGuildData')
-			.setLabel('Cancel')
-			.setStyle(ButtonStyle.Secondary);
-		const row = new ActionRowBuilder()
-			.addComponents(confirm, cancel);
-
 		let embed = new EmbedBuilder();
 		embed.setColor('#FF0000');
 		embed.setTitle("Confirm deletion of all bot settings?");
 		embed.setDescription("This action is irreversible.");
-		const confirmationMsg = await interaction.editReply({embeds: [embed], components: [row]}); 
-
-		const collectorFilter = i => i.user.id === interaction.user.id; //make sure the incoming interaction cames from the same user who started the slash command
-		let resultEmbed = new EmbedBuilder();
-		try {
-			const confirmation = await confirmationMsg.awaitMessageComponent({ filter: collectorFilter, time: 60_000 }); //await response TODO TEST
-
-			if (confirmation.customId === 'confirmDeleteGuildData') {
+		await interaction.editReply({embeds: [embed]}); 
+		
+		commandConfirmation(interaction).then(async res => {
+			let resultEmbed = new EmbedBuilder();
+			if (res === 'confirmed') {
 				await guildProfileSchema.findOneAndDelete({guildId: interaction.guildId});	
-				console.log()
 				resultEmbed.setColor("#78F72F")
 				resultEmbed.setTitle("Deletion Successful!")
 				resultEmbed.setDescription("All server settings have been deleted.")
-			} else if (confirmation.customId === 'cancelDeleteGuildData') {
+			} else if (res === 'cancelled') {
 				resultEmbed.setColor("#FF0000");
 				resultEmbed.setTitle("Operation canceled. No changes were made.")
-			}
-			await confirmation.update({ embeds: [resultEmbed], components: [] });
-		} catch (e) {
-			if(e.code=="InteractionCollectorError"){ //probably a timeout? no specific error code for timeout, might have to change if Ifind that this code can occur with other errors.
+			} else if (res === 'timeout') {
 				resultEmbed.setColor("#FF0000");
 				resultEmbed.setTitle("Confirmation not recieved within 1 minute. No changes were made.");
-				resultEmbed.setTimestamp();
-				await interaction.editReply({ embeds: [resultEmbed], components: [] });
-			}else{
-				console.log(e);
-				resultEmbed.setColor("#FF0000");
-				resultEmbed.setTitle("Internal bot error occured. Please contact the developer and give the timestamp below");
-				resultEmbed.setTimestamp();
-				await interaction.editReply({ embeds: [resultEmbed], components: [] });
 			}
-		}
+			resultEmbed.setTimestamp();
+			await interaction.editReply({ embeds: [resultEmbed]});
+		})
 	} 
 }       
